40b0fc464d1cae373c94add6384c10557687771a
misostack <misostack.com@gmail.com>
2020-03-29 21:53:28 +0700

    Add static site serve with node

--- server/server.js
+++ server/server.js
@@ -5,10 +5,6 @@ const port = process.env.PORT || 3000;
 const publicPath = path.join(__dirname, '..', 'build');
 app.use(express.static(publicPath));
 
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
-
 app.listen(port, () => {
    console.log(`Server is up on port ${port}!`);
 });
